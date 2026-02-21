f6288ef42817e65f089aa751f98c79d187068cb9
SyncRao <shahrukhrao1@gmail.com>
2026-02-21 12:25:59 +0530

    added cart

--- components/ProductsCard.tsx
+++ components/ProductsCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { fetcher } from "../lib/fetcher";
+import { fetcher } from "@/app/lib/fetcher";
 import Link from "next/link";
 
 interface Product {
